47f45f45c09f203dc34c4c4769e9967cfb9e96e6
Antti Rask <antti.rask@teosto.fi>
2023-11-08 01:51:34 +0200

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# Install Quarto
-wget -qO- "https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.450/quarto-1.3.450-linux-amd64.deb" | sudo dpkg -i -
+
+# Download the Quarto binary
+wget -qO- "https://github.com/quarto-dev/quarto-cli/releases/download/v1.0.32/quarto-1.0.32-linux-amd64.deb" > quarto.deb
+
+# Install Quarto without sudo
+dpkg -x quarto.deb $HOME
+
+# Add Quarto to PATH
+export PATH=$HOME/usr/bin:$PATH
+
+# Verify Quarto installation
+quarto --version
+
 # Render the book
 quarto render index.qmd
-
