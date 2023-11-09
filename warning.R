5bb98f1e91f13710d8cdc3a7a7552a04a50c82af
Antti Rask <antti.rask@teosto.fi>
2023-11-09 10:49:58 +0200

    -Updated Welcome -Added warning note

---
+++ warning.R
@@ -0,0 +1,9 @@
+callout <- paste0(
+  "\n",
+  "::: callout-warning \n",
+  "## This book/site is under construction \n",
+  "Everything is liable to change, even the title \n",
+  "::: \n"
+)
+
+cat(callout)
