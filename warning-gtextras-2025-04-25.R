4a00a4f2a4076d2eb4eff8936fb7b979e4b6b2ed
Antti Rask <anttilennartrask@gmail.com>
2025-04-25 13:11:59 +0300

    -Better warning for the gtExtras bug

--- warning-gtextras-2025-04-25.R
+++ warning-gtextras-2025-04-25.R
@@ -1,10 +1,10 @@
 callout <- paste0(
   "\n",
   "::: callout-warning \n",
-  "## This code chunk isn't currently working (2025-04-25) \n",
-  "There is an issue with the package used. \n",
-  "While it's going on, its code will be commented out. \n",
-  "Will show a static image instead \n",
+  "## This code chunk isn't currently working (2025-04-25)
+  There is an issue with the package used and 
+  while it's going on, its code will be commented out. \n
+  Will show a static image instead. \n",
   "::: \n"
 )
 
