5f8a42cd21f7caf344d1d79b0092fdce88aea7cd
Antti Rask <anttilennartrask@gmail.com>
2026-04-04 02:55:45 +0300

    Rewrite of the 3.2.2 Mosaic chart replacing ggmosaic with marimekko.

--- warning-ggmosaic-2025-09-12.R
+++
@@ -1,9 +0,0 @@
-callout <- paste0(
-  "\n",
-  "::: callout-warning \n",
-  "## This package is waiting for an update \n",
-  "After ggplot2 4.0.0 update (2025-09-12) \n",
-  "::: \n"
-)
-
-cat(callout)
